a59e7abf305a9e0c877769aa9c60a5b23ddea3d0
knayecreator <78888215+knayecreator@users.noreply.github.com>
2021-04-04 13:09:30 -0400

    Delete events/guild directory

--- events/guild/guildMemberRemove.js
+++
@@ -1,9 +0,0 @@
-const db = require('quick.db');
-
-module.exports = (Discord, client, member) => {
-    const channell = db.get(`welcome_${member.guild.id}`)
-    const channel = member.guild.channels.cache.get(channell);
-    if (!channel) return;
-
-    channel.send(`${member} just left :(`)
-}
