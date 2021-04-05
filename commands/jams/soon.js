33264249e879248079e2ac65b91f0649f9518048
knayecreator <78888215+knayecreator@users.noreply.github.com>
2021-04-05 11:27:00 -0400

    Add files via upload

---
+++ commands/jams/soon.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'soon',
+    description: 'soon',
+    execute(client, message, args) {
+        message.channel.send('yzy will have music soon :eyes:')       
+    }
+}
