82c53fc97f6a6a07654cab65880490f77e96bfc6
knayecreator <78888215+knayecreator@users.noreply.github.com>
2021-04-05 15:17:28 -0400

    Delete commands/jams directory

--- commands/jams/soon.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'soon',
-    description: 'soon',
-    execute(client, message, args) {
-        message.channel.send('yzy will have music soon :eyes:')       
-    }
-}
