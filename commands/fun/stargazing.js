840884be26ca1254d4a1ba99d8631461dbd3ca0a
knayecreator <78888215+knayecreator@users.noreply.github.com>
2021-04-11 13:07:01 -0400

    Delete commands directory

--- commands/fun/stargazing.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'stargaze',
-    description: 'stargaze',
-    execute(client, message, args) {
-        message.channel.send('☄︎. *. ☄︎. *. ⋆\n☄︎. *. ⋆︎. \n*. ⋆⋆·˚ ༘ * :telescope::bug:')       
-    }
-}
