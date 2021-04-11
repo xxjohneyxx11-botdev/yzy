67513f681c3812e008add57770e149040a73c66b
knayecreator <78888215+knayecreator@users.noreply.github.com>
2021-04-11 13:05:58 -0400

    Delete commands/fun directory

--- commands/fun/maybike.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'maybike',
-    aliases: ['funny'],
-    description: 'maybike',
-    execute(client, message, args) {
-        message.channel.send('https://www.youtube.com/watch?v=w0TXsFjLmlw')       
-    }
-}
