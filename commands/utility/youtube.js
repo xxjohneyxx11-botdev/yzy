95b14e98a5d423d50e18f2587c9cf7eaea1f9901
knayecreator <78888215+knayecreator@users.noreply.github.com>
2021-04-12 10:30:04 -0400

    Update youtube.js

--- commands/utility/youtube.js
+++ commands/utility/youtube.js
@@ -12,7 +12,7 @@ module.exports = {
     aliases: ['yt'],
     description: 'youtube <video>',
     async execute(client, message, args) {
-        const apiKey = 'AIzaSyAx9nkSt2wid8_5JclJUuaC7Gh3AEcFF7M'
+        const apiKey = ''
         const videoName = args.join(' ');
         if (!videoName) return message.channel.send(embed3)
         const searchOptions = {
